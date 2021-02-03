a13f0b086b0a51c14047d74f8fa30729ebae7459
EC2 Default User <ec2-user@ip-172-31-48-203.ec2.internal>
2021-02-03 17:24:23 +0000

    Added Artist class to artist.py, added random song display to app.py and default error display, modified index.html to show song information and a refresh button, modified styles.css

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, redirect, request
+import json
+from artist import Artist, artist_ids
+import random
 import os
 
 app = Flask(__name__)
@@ -6,7 +9,20 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    try:
+        if request.method == 'POST' and "refresh" in request.form:
+            i = random.randint(0, len(artist_ids)-1)
+            artist = Artist(i)
+            print(artist.random_track())
+        else:
+            i = random.randint(0, len(artist_ids)-1)
+            artist = Artist(i)
+            print(artist.random_track())
+        return render_template('index.html', artist=artist.random_track())
+    except:
+        track_info = {"artist": "Pink Fong", "album": "Baby Shark", "track": "Baby Shark", "image": "https://cdns-images.dzcdn.net/images/cover/9d42e4daa95703c8665b72208d7b11fe/264x264.jpg", "preview": "/static/baby_shark.mp3"}
+        return render_template('index.html', artist=track_info)
+    
 
 if __name__ == "__main__":
     app.run(port = int(os.getenv('PORT', 5000)),
--- artist.py
+++ artist.py
@@ -2,6 +2,60 @@ import requests
 import json
 import urllib.request as urllib2
 import random
+import os
 from dotenv import load_dotenv, find_dotenv
 
+artist_ids = [["The Weeknd", "1Xyo4u8uXC1ZmMpatF05PJ"], ["ASAP Rocky", "13ubrt8QOOCPljQ2FL1Kca"], ["Drake", "3TVXtAsR1Inumwj472S9r4"], ["Post Malone", "246dkjvS1zLTtiykXe5h60"], ["Lil Baby","5f7VJjfbwm532GiveGC0ZK"], ["DaBaby", "4r63FhuTkUYltbVAg5TQnk"], ["Roddy Ricch", "757aE44tKEUQEqRuT6GnEB"]]
+
 load_dotenv(find_dotenv())
+
+def refresh():
+    query = "https://accounts.spotify.com/api/token"
+    response = requests.post(query, data={"grant_type": "client_credentials"},
+                headers= {"Authorization"
+                 : "Basic " + os.getenv('base64')})
+    token = response.json()['access_token']
+    return token
+
+def song_preview(_id_, tok):
+    query = "https://api.spotify.com/v1/tracks/{}?market=US".format(_id_)
+    response = requests.get(query, headers={"Content-Type": "application/json",
+                                            "Authorization": "Bearer {}".format(tok)})
+    response_json = response.json()
+    #print(response_json)
+    prev = response_json['preview_url']
+    #print(prev)
+    return prev
+
+class Artist:
+    def __init__(self, num=None, song=None, artist=None):
+        self.num = num if num is not None else None
+        self.song = song if song is not None else None
+        self.artist = artist if artist is not None else None
+
+    def top_track(self, response_json, token):
+        tracks_lst = response_json["tracks"] #Big list of tracks
+        num = random.randint(0, len(tracks_lst)-1) #Picks track in json file
+        song = tracks_lst[num]
+        artist = song["album"]["artists"][0]["name"]
+        album = song["album"]["name"]
+        track = song["name"]
+        type = song["type"]
+        image = song["album"]["images"][0]["url"]
+        preview = song_preview(song["id"], token)
+        #song name, song artist, song-related image, song preview URL
+        track_info = {"artist": artist, "album": album, "track": track, "image": image, "preview": preview}
+        return track_info
+
+    def random_track(self):
+        artist_id = artist_ids[self.num][1]
+        query = "https://api.spotify.com/v1/artists/{}/top-tracks?market=US".format(artist_id)
+        token = refresh()
+        response = requests.get(query, headers={"Content-Type": "application/json",
+                                                "Authorization": "Bearer {}".format(token)})
+        response_json = response.json()
+        track_info = self.top_track(response_json, token)
+        return track_info
+        
+#artist = Artist(3)
+#print(artist.random_track())
