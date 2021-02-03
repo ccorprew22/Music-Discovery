1a8ad77894c262dde248f166d9f2b143fad2725c
EC2 Default User <ec2-user@ip-172-31-48-203.ec2.internal>
2021-02-03 17:33:52 +0000

    Modified app.py to print correct song information to console, added files to static directory

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ def index():
         if request.method == 'POST' and "refresh" in request.form:
             i = random.randint(0, len(artist_ids)-1)
             artist = Artist(i)
-            print(artist.random_track())
+            a = artist.random_track()
+            print(a)
         else:
             i = random.randint(0, len(artist_ids)-1)
             artist = Artist(i)
-            print(artist.random_track())
-        return render_template('index.html', artist=artist.random_track())
+            a = artist.random_track()
+            print(a)
+        return render_template('index.html', artist=a)
     except:
+        print("Baby Shark")
         track_info = {"artist": "Pink Fong", "album": "Baby Shark", "track": "Baby Shark", "image": "https://cdns-images.dzcdn.net/images/cover/9d42e4daa95703c8665b72208d7b11fe/264x264.jpg", "preview": "/static/baby_shark.mp3"}
         return render_template('index.html', artist=track_info)
     
